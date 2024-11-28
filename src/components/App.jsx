29c261c629a5e3dbb708d90a958a610c04ac85e4
Olena Deynega <suntrotter@gmail.com>
2024-11-28 10:00:53 +0200

    corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,59 +6,54 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(() => {
-    return Number(localStorage.getItem('good')) || 0;
-  });
-  const [bad, setBad] = useState(() => {
-    return Number(localStorage.getItem('bad')) || 0;
-  });
-  const [neutral, setNeutral] = useState(() => {
-    return Number(localStorage.getItem('neutral')) || 0;
+  const [feedback, setFeedback] = useState(() => {
+    return {
+      good: Number(localStorage.getItem('good')) || 0,
+      neutral: Number(localStorage.getItem('neutral')) || 0,
+      bad: Number(localStorage.getItem('bad')) || 0
+    };
   });
 
- useEffect(() => {
-    localStorage.setItem('good', good);
-    localStorage.setItem('bad', bad);
-    localStorage.setItem('neutral', neutral);
-  }, [good, bad, neutral]);
+  useEffect(() => {
+    localStorage.setItem('good', feedback.good);
+    localStorage.setItem('neutral', feedback.neutral);
+    localStorage.setItem('bad', feedback.bad);
+  }, [feedback]);
 
-  const goodHandleClick = () => {
-    setGood(good + 1);
-  };
-
-  const badHandleClick = () => {
-    setBad(bad + 1);
-  };
-
-  const neutralHandleClick = () => {
-    setNeutral(neutral + 1);
+  const handleClick = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
   };
 
   const resetFeedback = () => {
-    setGood(0);
-    setBad(0);
-    setNeutral(0);
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
     localStorage.clear();
   };
 
-  const totalFeedback = good + neutral + bad;
-  const positiveFeedback = totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   return (
     <>
       <Description />
       <Options 
-        onGoodClick={goodHandleClick} 
-        onBadClick={badHandleClick} 
-        onNeutralClick={neutralHandleClick} 
+        onGoodClick={() => handleClick('good')} 
+        onBadClick={() => handleClick('bad')} 
+        onNeutralClick={() => handleClick('neutral')} 
         onResetClick={resetFeedback}
         totalFeedback={totalFeedback}
       />
       {totalFeedback > 0 ? (
         <Feedback 
-          good={good}
-          bad={bad}
-          neutral={neutral}
+          good={feedback.good}
+          bad={feedback.bad}
+          neutral={feedback.neutral}
           totalFeedback={totalFeedback}
           positiveFeedback={positiveFeedback}
         />
